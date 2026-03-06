ac405d5bc5e3a87b30228680d27fb75c53aef136
ssc19940105 <ssc19940105@163.com>
2026-03-07 00:19:59 +0800

    feat(dictionary): 添加字典对话框界面和数据表格功能
    - 集成Syncfusion数据网格组件用于显示字典条目
    - 添加导入XLF文件按钮和相关UI元素
    - 实现响应式网格布局支持行列定义
    - 配置设计时数据上下文绑定到视图模型
    - 扩展窗口尺寸以适应新的界面布局
    - 简化代码文件的命名空间引用结构

--- Witcher3StringEditor.Dialogs/Views/DictionaryDialog.xaml.cs
+++ Witcher3StringEditor.Dialogs/Views/DictionaryDialog.xaml.cs
@@ -1,27 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace Witcher3StringEditor.Dialogs.Views
+namespace Witcher3StringEditor.Dialogs.Views;
+
+/// <summary>
+///     DictionaryDialog.xaml 的交互逻辑
+/// </summary>
+public partial class DictionaryDialog : Window
 {
-    /// <summary>
-    /// DictionaryDialog.xaml 的交互逻辑
-    /// </summary>
-    public partial class DictionaryDialog : Window
+    public DictionaryDialog()
     {
-        public DictionaryDialog()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
