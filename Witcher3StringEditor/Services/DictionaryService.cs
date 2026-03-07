91faf62f3b44ac812560a4b6828c278b2a1d106e
ssc19940105 <ssc19940105@163.com>
2026-03-07 23:06:06 +0800

    feat(dictionary): 添加文件系统监控功能
    - 移除类级别XliffReader字段的初始化
    - 添加文件监视器的Changed事件处理程序
    - 实现FileWatcher_Changed方法处理文件变更事件
    - 添加HandleFileDeleted方法处理文件删除操作
    - 添加HandleFileCreated方法处理文件创建操作
    - 文件创建时自动读取XLIFF信息并添加到字典集合
    - 文件删除时从字典集合中移除对应的XLIFF信息

--- Witcher3StringEditor/Services/DictionaryService.cs
+++ Witcher3StringEditor/Services/DictionaryService.cs
@@ -7,15 +7,8 @@ namespace Witcher3StringEditor.Services;
 
 public class DictionaryService : IDictionaryService
 {
-    private readonly XliffReader xliffReader = new();
     private readonly FileSystemWatcher fileWatcher = new();
-    
-    public ObservableCollection<XliffInfo> Dictionaries { get; } = [];
-
-    public XliffDocument LoadDictionary(XliffInfo xliffInfo)
-    {
-        return xliffReader.ReadDocument(xliffInfo);
-    }
+    private readonly XliffReader xliffReader = new();
 
     public DictionaryService()
     {
@@ -25,5 +18,43 @@ public class DictionaryService : IDictionaryService
         fileWatcher.Filters.Add("*.xlf");
         fileWatcher.Filters.Add("*.xliff");
         fileWatcher.EnableRaisingEvents = true;
+        fileWatcher.Changed += FileWatcher_Changed;
+    }
+
+    public ObservableCollection<XliffInfo> Dictionaries { get; } = [];
+
+    public XliffDocument LoadDictionary(XliffInfo xliffInfo)
+    {
+        return xliffReader.ReadDocument(xliffInfo);
+    }
+
+    private void FileWatcher_Changed(object sender, FileSystemEventArgs e)
+    {
+        switch (e.ChangeType)
+        {
+            case WatcherChangeTypes.Created:
+                HandleFileCreated(e);
+                break;
+            case WatcherChangeTypes.Deleted:
+                HandleFileDeleted(e);
+                break;
+            case WatcherChangeTypes.Changed:
+            case WatcherChangeTypes.Renamed:
+            case WatcherChangeTypes.All:
+            default:
+                break;
+        }
+    }
+
+    private void HandleFileDeleted(FileSystemEventArgs e)
+    {
+        var xliffInfo = Dictionaries.FirstOrDefault(x => x.FilePath == e.FullPath);
+        if (xliffInfo != null) Dictionaries.Remove(xliffInfo);
+    }
+
+    private void HandleFileCreated(FileSystemEventArgs e)
+    {
+        var xliffInfo = xliffReader.ReadInfo(e.FullPath);
+        if (xliffInfo != null) Dictionaries.Add(xliffInfo);
     }
 }
