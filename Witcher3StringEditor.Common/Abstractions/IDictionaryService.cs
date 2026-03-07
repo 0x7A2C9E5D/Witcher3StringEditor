e7ff49feb97fd1469132a4c1ce507f5c16353e5f
ssc19940105 <ssc19940105@163.com>
2026-03-07 22:56:26 +0800

    feat(services): 添加字典服务实现
    - 创建 DictionaryService 类实现 IDictionaryService 接口
    - 集成 XliffReader 用于加载字典文档
    - 添加文件监视器以监控字典文件变化
    - 在 Common 项目中添加对 Xliff 项目的引用
    - 定义 Dictionaries 属性存储字典集合
    - 实现 LoadDictionary 方法加载指定字典文件

---
+++ Witcher3StringEditor.Common/Abstractions/IDictionaryService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+using Witcher3StringEditor.Xliff;
+
+namespace Witcher3StringEditor.Common.Abstractions;
+
+public interface IDictionaryService
+{
+    public ObservableCollection<XliffInfo> Dictionaries { get; }
+
+    public XliffDocument LoadDictionary(XliffInfo xliffInfo);
+}
---
+++ Witcher3StringEditor/Services/DictionaryService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.ObjectModel;
+using System.IO;
+using Witcher3StringEditor.Common.Abstractions;
+using Witcher3StringEditor.Xliff;
+
+namespace Witcher3StringEditor.Services;
+
+public class DictionaryService : IDictionaryService
+{
+    private readonly XliffReader xliffReader = new();
+    private readonly FileSystemWatcher fileWatcher = new();
+    
+    public ObservableCollection<XliffInfo> Dictionaries { get; } = [];
+
+    public XliffDocument LoadDictionary(XliffInfo xliffInfo)
+    {
+        return xliffReader.ReadDocument(xliffInfo);
+    }
+
+    public DictionaryService()
+    {
+        fileWatcher.Path = string.Empty;
+        fileWatcher.NotifyFilter = NotifyFilters.CreationTime | NotifyFilters.LastWrite | NotifyFilters.FileName |
+                                   NotifyFilters.Size;
+        fileWatcher.Filters.Add("*.xlf");
+        fileWatcher.Filters.Add("*.xliff");
+        fileWatcher.EnableRaisingEvents = true;
+    }
+}
