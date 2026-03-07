4db5a2ded12f860f2eaf48d966edc036e880d12e
ssc19940105 <ssc19940105@163.com>
2026-03-07 11:48:07 +0800

    feat(xliff): 添加XLIFF读取器接口和文档信息功能
    - 在IXliffReader接口中添加ReadInfo方法用于读取XLIFF文档信息
    - 添加ReadDocument方法支持从文件路径或XliffInfo对象读取文档
    - 在XliffInfo类中添加FilePath属性用于存储文件路径信息

--- Witcher3StringEditor.Xliff/XliffInfo.cs
+++ Witcher3StringEditor.Xliff/XliffInfo.cs
@@ -5,6 +5,8 @@ namespace Witcher3StringEditor.Xliff;
 [PublicAPI]
 public class XliffInfo
 {
+    public required string FilePath { get; init; }
+    
     public required Version Version { get; init; }
 
     public required string SourceLanguage { get; init; }
