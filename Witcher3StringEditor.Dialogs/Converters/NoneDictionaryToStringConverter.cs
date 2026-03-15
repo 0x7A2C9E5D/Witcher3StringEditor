c2e0f00ac52b0fcb5fe6cc5fdc05b8ac0ec36d34
ssc19940105 <ssc19940105@163.com>
2026-03-15 12:04:03 +0800

    refactor(dialogs): 重构字典名称显示转换器
    - 将 PathToDisplayNameConverter 重命名为 NoneDictionaryToStringConverter
    - 修改转换逻辑从文件路径提取改为直接使用字典名称
    - 更新 XAML 中的数据绑定从 Path 属性改为 Name 属性
    - 移除对 System.IO 的引用依赖
    - 简化字符串空值检查逻辑

--- Witcher3StringEditor.Dialogs/Converters/NoneDictionaryToStringConverter.cs
+++ Witcher3StringEditor.Dialogs/Converters/NoneDictionaryToStringConverter.cs
@@ -1,12 +1,11 @@
 ﻿using System.Globalization;
-using System.IO;
 using System.Windows;
 using System.Windows.Data;
 using Witcher3StringEditor.Locales;
 
 namespace Witcher3StringEditor.Dialogs.Converters;
 
-public class PathToDisplayNameConverter : IValueConverter
+public class NoneDictionaryToStringConverter : IValueConverter
 {
     /// <summary>
     ///     Converts a file path to a file name
@@ -18,11 +17,8 @@ public class PathToDisplayNameConverter : IValueConverter
     /// <returns></returns>
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is not string filePath)
-            return DependencyProperty.UnsetValue;
-        return filePath == string.Empty
-            ? Strings.NoDictionary!
-            : Path.GetFileNameWithoutExtension(filePath);
+        if (value is not string name) return DependencyProperty.UnsetValue;
+        return string.IsNullOrWhiteSpace(name) ? Strings.NoDictionary : name;
     }
 
     /// <summary>
