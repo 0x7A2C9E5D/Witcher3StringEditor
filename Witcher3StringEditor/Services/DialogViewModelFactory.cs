7561198406266d26c9ec0631a36dfca5a2a8a09d
ssc19940105 <ssc19940105@163.com>
2026-09-07 08:22:06 +0800

    docs(services): 补充 DialogViewModelFactory 的 XML 注释

--- Witcher3StringEditor/Services/DialogViewModelFactory.cs
+++ Witcher3StringEditor/Services/DialogViewModelFactory.cs
@@ -19,19 +19,30 @@ namespace Witcher3StringEditor.Services;
 /// <param name="serviceProvider">The service provider used to resolve view model dependencies</param>
 internal sealed class DialogViewModelFactory(IServiceProvider serviceProvider) : IDialogViewModelFactory
 {
-    /// <inheritdoc />
+    /// <summary>
+    ///     Creates a new instance of the EditDataDialogViewModel class
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
     public EditDataDialogViewModel CreateEditDialog(ITrackableW3StringItem item)
     {
         return new EditDataDialogViewModel(item);
     }
 
-    /// <inheritdoc />
+    /// <summary>
+    ///     Creates a new instance of the DeleteDataDialogViewModel class
+    /// </summary>
+    /// <param name="items"></param>
+    /// <returns></returns>
     public DeleteDataDialogViewModel CreateDeleteDialog(IEnumerable<IW3StringItem> items)
     {
         return new DeleteDataDialogViewModel(items);
     }
 
-    /// <inheritdoc />
+    /// <summary>
+    ///     Creates a new instance of the BackupDialogViewModel class
+    /// </summary>
+    /// <returns></returns>
     public BackupDialogViewModel CreateBackupDialog()
     {
         return new BackupDialogViewModel(
@@ -40,7 +51,12 @@ internal sealed class DialogViewModelFactory(IServiceProvider serviceProvider) :
             serviceProvider.GetRequiredService<IDialogService>());
     }
 
-    /// <inheritdoc />
+    /// <summary>
+    ///     Creates a new instance of the SaveDialogViewModel class
+    /// </summary>
+    /// <param name="items"></param>
+    /// <param name="outputDirectory"></param>
+    /// <returns></returns>
     public SaveDialogViewModel CreateSaveDialog(IReadOnlyList<IW3StringItem> items, string outputDirectory)
     {
         return new SaveDialogViewModel(
@@ -51,13 +67,20 @@ internal sealed class DialogViewModelFactory(IServiceProvider serviceProvider) :
             outputDirectory);
     }
 
-    /// <inheritdoc />
+    /// <summary>
+    ///     Creates a new instance of the LogDialogViewModel class
+    /// </summary>
+    /// <returns></returns>
     public LogDialogViewModel CreateLogDialog()
     {
         return new LogDialogViewModel(serviceProvider.GetRequiredService<ILogAccessService>());
     }
 
-    /// <inheritdoc />
+    /// <summary>
+    ///     Creates a new instance of the SettingsDialogViewModel class
+    /// </summary>
+    /// <param name="translatorNames"></param>
+    /// <returns></returns>
     public SettingDialogViewModel CreateSettingsDialog(IEnumerable<string> translatorNames)
     {
         return new SettingDialogViewModel(
@@ -68,21 +91,35 @@ internal sealed class DialogViewModelFactory(IServiceProvider serviceProvider) :
             serviceProvider.GetRequiredService<ICultureResolver>().SupportedCultures);
     }
 
-    /// <inheritdoc />
+    /// <summary>
+    ///     Creates a new instance of the AboutDialogViewModel class
+    /// </summary>
+    /// <param name="aboutInfo"></param>
+    /// <returns></returns>
     public AboutDialogViewModel CreateAboutDialog(IReadOnlyDictionary<string, object?> aboutInfo)
     {
         return new AboutDialogViewModel(aboutInfo);
     }
-
-    /// <inheritdoc />
+    
+    /// <summary>
+    ///     Creates a new instance of the RecentDialogViewModel class
+    /// </summary>
+    /// <returns></returns>
     public RecentDialogViewModel CreateRecentDialog()
     {
         return new RecentDialogViewModel(
             serviceProvider.GetRequiredService<IRecentFilesService>(),
             serviceProvider.GetRequiredService<IDialogService>());
     }
-
-    /// <inheritdoc />
+    
+    /// <summary>
+    ///     Creates a new instance of the TranslationDialogViewModel class
+    /// </summary>
+    /// <param name="translator"></param>
+    /// <param name="items"></param>
+    /// <param name="index"></param>
+    /// <param name="dictionaryService"></param>
+    /// <returns></returns>
     public TranslationDialogViewModel CreateTranslationDialog(ITranslator translator,
         IReadOnlyList<ITrackableW3StringItem> items, int index, IDictionaryService? dictionaryService)
     {
@@ -95,7 +132,10 @@ internal sealed class DialogViewModelFactory(IServiceProvider serviceProvider) :
             dictionaryService);
     }
 
-    /// <inheritdoc />
+    /// <summary>
+    ///     Creates a new instance of the BatchItemsTranslationViewModel class
+    /// </summary>
+    /// <returns></returns>
     public DictionaryManagerDialogViewModel CreateDictionaryManagerDialog()
     {
         return new DictionaryManagerDialogViewModel(
