df0b655450d3719fcc1c71cf293919d110c54b46
ssc19940105 <ssc19940105@163.com>
2026-09-21 15:28:40 +0800

    docs: 更新 DictionaryGroup 类的 XML 注释

--- Witcher3StringEditor.Dialogs/Models/DictionaryGroup.cs
+++ Witcher3StringEditor.Dialogs/Models/DictionaryGroup.cs
@@ -6,17 +6,19 @@ using Witcher3StringEditor.Dictionary;
 namespace Witcher3StringEditor.Dialogs.Models;
 
 /// <summary>
-///     A class representing a group of dictionaries
+///     A class representing a group of dictionaries that share a target language
 /// </summary>
+/// <param name="targetLanguage">The target language of the dictionaries in this group</param>
+/// <param name="dictionaries">The dictionaries of this group</param>
 public class DictionaryGroup(CultureInfo targetLanguage, List<DictionaryInfo> dictionaries)
 {
     /// <summary>
-    ///     Initializes a new instance of the DictionaryGroup class
+    ///     Gets the target language of the dictionaries in this group
     /// </summary>
     public CultureInfo TargetLanguage { get; } = targetLanguage;
 
     /// <summary>
-    ///     Initializes a new instance of the DictionaryGroup class
+    ///     Gets the dictionaries of this group as an observable collection
     /// </summary>
     public ObservableCollection<DictionaryInfo> Dictionaries { get; } = dictionaries.ToObservableCollection();
 }
