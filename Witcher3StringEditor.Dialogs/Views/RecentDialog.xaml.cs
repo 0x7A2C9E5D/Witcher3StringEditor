553ba67853e96293562cb9419a4a19a489c48fb4
ssc19940105 <ssc19940105@163.com>
2026-09-21 14:23:00 +0800

    refactor(recent): 移除记录删除确认行为及相关消息处理

--- Witcher3StringEditor.Dialogs/Views/RecentDialog.xaml.cs
+++ Witcher3StringEditor.Dialogs/Views/RecentDialog.xaml.cs
@@ -1,11 +1,4 @@
-﻿using System.Windows;
-using CommunityToolkit.Mvvm.Messaging;
-using CommunityToolkit.Mvvm.Messaging.Messages;
-using Witcher3StringEditor.Locales;
-using Witcher3StringEditor.Messaging;
-using MessageBox = iNKORE.UI.WPF.Modern.Controls.MessageBox;
-
-namespace Witcher3StringEditor.Dialogs.Views;
+﻿namespace Witcher3StringEditor.Dialogs.Views;
 
 /// <summary>
 ///     Interaction logic for RecentDialog.xaml
@@ -20,21 +13,5 @@ public partial class RecentDialog
     public RecentDialog()
     {
         InitializeComponent(); // Initialize the UI components
-        RegisterMessageHandler(); // Register message handler
-    }
-
-    /// <summary>
-    ///     Registers the message handler for the recent dialog
-    /// </summary>
-    private void RegisterMessageHandler()
-    {
-        WeakReferenceMessenger.Default.Register<RecentDialog, AsyncRequestMessage<bool>, string>(
-            this, MessageTokens.RecentItem, (_, m) =>
-            {
-                m.Reply(MessageBox.Show(Strings.RecordDeletingMessgae,
-                    Strings.RecordDeletingCaption,
-                    MessageBoxButton.YesNo,
-                    MessageBoxImage.Warning) == MessageBoxResult.Yes);
-            });
     }
 }
