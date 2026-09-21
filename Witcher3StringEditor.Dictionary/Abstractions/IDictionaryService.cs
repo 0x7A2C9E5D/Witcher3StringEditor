f40ac7bb4ba3470236d20349e439f4f43e8f4813
ssc19940105 <ssc19940105@163.com>
2026-09-21 17:10:09 +0800

    Revert "docs: 完善 IDictionaryService 接口文档注释"
    This reverts commit 4d78521281253f04d0df0e7d3f0d4be3687e252a.

--- Witcher3StringEditor.Dictionary/Abstractions/IDictionaryService.cs
+++ Witcher3StringEditor.Dictionary/Abstractions/IDictionaryService.cs
@@ -1,13 +1,6 @@
 ﻿namespace Witcher3StringEditor.Dictionary.Abstractions;
 
 /// <summary>
-///     A composite service that aggregates the dictionary capabilities: reading dictionary information and
-///     entries (<see cref="IDictionaryProvider" />), importing, removing and finding dictionaries
-///     (<see cref="IDictionaryManager" />), and binding a dictionary for dynamic replacement
-///     (<see cref="IDynamicDictionaryReplacer" />)
+///     A service that provides dictionary information and entries.
 /// </summary>
-/// <remarks>
-///     Consumers that only need dynamic replacement should depend on <see cref="IDynamicDictionaryReplacer" />
-///     directly, so they are not coupled to the import/remove/query surface they never use
-/// </remarks>
 public interface IDictionaryService : IDictionaryProvider, IDictionaryManager, IDynamicDictionaryReplacer;
